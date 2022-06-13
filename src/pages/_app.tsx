04dd10c734d1ab10f9fa55daac367e2fbf142f3d
Anderson Monteiro <derson1509@gmail.com>
2022-06-13 11:54:33 -0300

    Moving UI folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { AppProps } from "../../node_modules/next/app";
 
-import GlobalStyle from "./ui/styles/global";
-import theme from './ui/themes/theme';
+import GlobalStyle from "../ui/styles/global";
+import theme from '../ui/themes/theme';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) =>{
   return (
